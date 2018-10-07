770289ebd0a96aeaa833f6be37d4b2270915cd50
Joey Costello <joeyc.swim@gmail.com>
2018-10-07 12:19:30 -0400

    updated code, new functions

--- fullSystem.cpp
+++ fullSystem.cpp
@@ -40,77 +40,66 @@ const int downButtonPin = 29;
 const int teensyAddress = 7;
 const int sentBytes = 128;
 
-int fd;
+int lcd;
 
 
 
+// function declarations
 void waitForStart();
 void updateWaitingScreen();
+void setupPins();
+string int2str(int num);
 
-// temp
-void lcdPrint(const char *words1,const char *words2,const char *words3, const char *words4);
+
 
 int main() {
-/////////////////////////////////// ON BOOT ///////////////////////////////////////
+	/////////////////////////////////// ON BOOT ///////////////////////////////////////
 	// setup GPIO inputs and LCD
 	wiringPiSetup();
-	fd = lcdInit(4,20,4, 2,3, 6,5,4,1,0,0,0,0);
-
-	// setup button pins
-	pinMode(startButtonPin, INPUT);
-	pinMode(upButtonPin, INPUT);
-	pinMode(downButtonPin, INPUT);
-	pullUpDnControl(startButtonPin, PUD_DOWN);
-	pullUpDnControl(upButtonPin, PUD_DOWN);
-	pullUpDnControl(downButtonPin, PUD_DOWN);
+	setupPins();
+    lcd = setupLCD();
 
 	// Display title text
-	lcdClear(fd);
-	lcdPrint("","Clear Lung Project","","");
+	lcdClear(lcd);
+	lcdPrint(lcd,"","Clear Lung Project","","");
 	delay(2000);
 
-	// Display option to select recording time
-	// update the display on button presses
-	// wait for start button press
+    // Open output files for each mic
+	ofstream mic1;
+	mic1.open("mic1.txt");
+	ofstream mic2;
+	mic2.open("mic2.txt");
+
+	// Display option to select recording time and wait for start button press 
 	waitForStart();
 
-	cout << "done waiting for start" << endl;
-	lcdClear(fd);
-	lcdPrint("Recording now","","","");
 
 
+	/////////////////////////////////// ON START ///////////////////////////////////////
+	cout << "Recording now" << endl;
+	lcdClear(lcd);
+	lcdPrint(lcd,"Recording now","","","");
 
+    // setup teensy communication
+	Pi2c teensy1(teensyAddress);
+	
+    // send teensy the recording time (teensy starts recording)
+    int recordTimeMS = recordTimeSec * 1000;
+    teensy1.i2cWrite(int2str(recordTimeMS).cstr(), (int2str(recordTimeMS).length()+1)); 
 
+    // wait slightly longer than recording time
+	delay(1000*recordTimeSec + 500);
 
-	////////// Start recording - ***** NEEDS TO BE TESTED ***   //////////////////////////
-    Pi2c ard(teensyAddress); //look at address for i2c communications
-    char recieve[sentBytes]; //temporary location to hold recieved bytes
-    char recordingTime[8] = "300";
-    const int recTime_Length = 3;
-	// Open output file
-    ofstream os; 
-    os.open("output.txt");
-    assert(os.is_open());
-    // Send time to run
-    cout << "Recording for " << atoi(recordingTime)/1000 << " milliseconds" << std::endl;
-    ard.i2cWrite(recordingTime, recTime_Length); 
-    // Wait for said time
-    usleep((unsigned int)atoi(recordingTime));
-
-
-
-    // TEMP: delay a little more to let teensy setup
-    delay(1000);
-
-
+	/*
+	char recieve[chunkSize]; //temporary location to hold recieved bytes
     // Loop for reading multiple chunks of data
-    for (int j=0; j<6; j++) {
+    for (int j=0; j<4; j++) {
         // Read a chunk from i2c
         cout << "Reading data" << std::endl;
-        ard.i2cRead(recieve, sentBytes);
+        teensy1.i2cRead(recieve, chunkSize);
         // Print data to file
         cout << "Printing to file" << endl;
-        for (int i = 0; i < sentBytes; i += 2) { 
+        for (int i = 0; i < chunkSize; i += 2) { 
             unsigned char lower = recieve[i];
             unsigned char upper = recieve[i+1];
             unsigned short value = (upper << 8) | lower;
@@ -120,19 +109,40 @@ int main() {
         usleep(600);
     }
     os.close();
+	*/
+
+/*
+	// calculate total number of bytes for the recording time
+	int numMics = 2;
+	int totalBytes = 44100 * recordTimeSec * 2 * numMics;
+
+	// request the total num bytes from teensy in increments of (128)
+	// separate data into the files, write to files
+	int mic1Counter = 0;
+	int mic2Counter = 0;
+	for (int j=0; j< totalBytes/chunkSize; j++) { // may need to adjust the upper loop limit
+		receiveData(mic1, teensy1, chunkSize, &mic1Counter);
+        usleep(600); // delay to let teensy read data
+        receiveData(mic2, teensy1, chunkSize, &mic2Counter);
+        usleep(600); // delay to let teensy read data
+	}
+	mic1.close();
+	mic2.close();
 
 
 
+	/////////////////////////////////// ON FINISH ///////////////////////////////////////
+	lcdClear(lcd);
+	lcdPrint(lcd,"Finished Recording","","","");
 
+	/*
+	// show graph of 1 sec of recording
+	makeGraph("mic1.txt");
 
-
-
-    // Done Recording
-	lcdClear(fd);
-	lcdPrint("Finished Recording","","","");
-
-
-
+	// show graph of fft
+	runFFT(nearestPow2(valueCounter), "mic2.txt", "fft.txt");
+	makeGraph("fft.txt");
+	*/
 
 	return 0;
 }
@@ -140,6 +150,14 @@ int main() {
 
 
 
+void setupPins() {
+	pinMode(startButtonPin, INPUT);
+	pinMode(upButtonPin, INPUT);
+	pinMode(downButtonPin, INPUT);
+	pullUpDnControl(startButtonPin, PUD_DOWN);
+	pullUpDnControl(upButtonPin, PUD_DOWN);
+	pullUpDnControl(downButtonPin, PUD_DOWN);
+}
 
 
 void waitForStart() {
@@ -166,27 +184,33 @@ void waitForStart() {
 }
 
 void updateWaitingScreen() {
-			stringstream strs;
-  			strs << recordTimeSec;
-  			string lengthString = strs.str() + " sec";
-			lcdClear(fd);
-			lcdPrint("Record for", lengthString.c_str(), "Press start", "");
+	string lengthString = int2str(recordTimeSec) + " sec";
+	lcdClear(lcd);
+	lcdPrint(lcd,"Record for", lengthString.c_str(), "Press start", "");
+}
 
+string int2str(int num) {
+	stringstream strs;
+	strs << num;
+	return strs.str();
 }
 
 
-
-void lcdPrint(const char *words1,const char *words2,const char *words3, const char *words4) {
-	//int fd = lcdInit(4,20,4, 2,3, 6,5,4,1,0,0,0,0);
-	std::vector <const char*> word_vector;
-	word_vector.push_back(words1);
-	word_vector.push_back(words2);
-	word_vector.push_back(words3);
-	word_vector.push_back(words4);
-	for(int line = 0; line < 4; ++line){
-		lcdPosition(fd, 0, line);
-		lcdPuts(fd, word_vector[line]);
+// function reads in data from a teensy and writes to a file
+void receiveData(ifstream &outfile, Pi2c &teensy, int bufSize, int *valueCounter) {
+	char recieve[bufSize]; 
+	teensy.i2cRead(recieve, bufSize);
+	// convert from bytes to integer, write to file
+	for (int i=0; i < bufSize; i+=2) {
+		unsigned char lower = recieve[i];
+		unsigned char upper = recieve[i+1];
+		unsigned short value = (upper << 8) | lower;
+		if (value > 10000){
+			// subtract 2^16
+			value -= 65536; 
+		}
+		outfile << *valueCounter << " " value << endl;
+		(*valueCounter)++;
 	}
 }
-
 
